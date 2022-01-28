02b38fde135e4cdbca192484eaf772cb7f180474
Javi <javi@jsrois.net>
2022-01-28 15:27:31 +0100

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import {cleanup, fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+test('cuando se carga la página, el contador empieza en cero', () => {
+  render(<App />)
+
+  const contador = screen.getByText(/La cuenta es 0/i);
+
+  expect(contador).toBeInTheDocument();
 });
+
+test('cuando hacemos click en el botón, se incrementa el contador', () => {
+  render(<App />)
+
+  const boton = screen.getByText(/incrementar/i);
+
+  fireEvent.click(boton);
+
+  const contador = screen.getByText(/la cuenta es 1/i);
+
+  expect(contador).toBeInTheDocument();
+});
+
+test('el contador se reinicia al llegar a 10', () => {
+  render(<App />)
+
+  const boton = screen.getByText(/incrementar/i);
+
+  for (let i = 0; i < 10; i++) {
+    fireEvent.click(boton);
+  }
+
+  const contador = screen.getByText(/la cuenta es 0/i);
+  expect(contador).toBeInTheDocument();
+});
+
+
